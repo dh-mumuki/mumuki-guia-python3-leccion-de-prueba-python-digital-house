df40861273bf76ed7200988437bc59de640f366c
franco@mumuki.org <franco@mumuki.org>
2021-03-11 21:12:39 +0000

    Mumuki Export on 2021-03-11 21:12:39 +0000

--- 00001_Sumar/test.py
+++ 00001_Sumar/test.py
@@ -1,4 +1,4 @@
 class Test(unittest.TestCase):
 
   def test_suma_de_1_y_2_es_3(self):
-    self.assertEqual(sumar(1, 2), 3, "sumar(1, 2) debería devolver 3 pero devolvió " + sumar(1, 2))
+    self.assertEqual(sumar(1, 2), 3, "sumar(1, 2) debería devolver 3 pero devolvió " + str(sumar(1, 2)))
