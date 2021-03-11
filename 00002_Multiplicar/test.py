324f470ce4fd873d97b056ffd7550b561bd33683
franco@mumuki.org <franco@mumuki.org>
2021-03-11 21:32:17 +0000

    Mumuki Export on 2021-03-11 21:32:17 +0000

--- 00002_Multiplicar/test.py
+++ 00002_Multiplicar/test.py
@@ -1,7 +1,7 @@
 class Test(unittest.TestCase):
 
   def test_multiplicar_3_por_1_tercio_devuelve_1(self):
-    self.assertEqual(multiplicar(3, 1/3), 1, f"multiplicar(3,1/3) debería devolver 1, pero devolvió {multiplicar(3, 1/3)}")
+    self.assertEqual(multiplicar(3, 1/3), 1, 0.01, f"multiplicar(3,1/3) debería devolver 1, pero devolvió {multiplicar(3, 1/3)}")
 
   def test_multiplicar_4_por_5_devuelve_20(self):
     self.assertEqual(multiplicar(4, 5), 20, f"multiplicar(4,5) debería devolver 20, pero devolvió {multiplicar(4, 5)}")
