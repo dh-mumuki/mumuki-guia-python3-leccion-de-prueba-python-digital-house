5b1efab8e2c568d5f428d7dac3f1966b52007776
franco@mumuki.org <franco@mumuki.org>
2021-03-11 21:28:56 +0000

    Mumuki Export on 2021-03-11 21:28:55 +0000

--- 00002_Multiplicar/description.md
+++ 00002_Multiplicar/description.md
@@ -2,7 +2,6 @@
 > Definí la función `multiplicar` que toma por argumento dos números y devuelve el producto de ellos. 
 
 
-def test_magnitude
 def test_positive_times_negative
 def test_negative_times_negetive
 def test_positive_times_positive
--- 00002_Multiplicar/test.py
+++ 00002_Multiplicar/test.py
@@ -2,3 +2,6 @@ class Test(unittest.TestCase):
 
   def test_multiplicar_4_por_5_devuelve_20(self):
     self.assertEqual(multiplicar(4, 5), 20, f"multiplicar(4,5) debería devolver 20, pero devolvió {multiplicar(4, 5)}")
+    
+  def test_multiplicar_4_por_menos_5_devuelve_menos_20(self):
+    self.assertEqual(multiplicar(4, -5), -20, f"multiplicar(4, -5) debería devolver -20, pero devolvió {multiplicar(4, -5)}")
