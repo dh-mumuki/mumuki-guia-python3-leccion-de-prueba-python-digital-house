f0945b480cfed6ffc2aff4057c0456759b88278d
gustavo@mumuki.org <gustavo@mumuki.org>
2021-03-11 22:38:07 +0000

    Mumuki Export on 2021-03-11 22:38:07 +0000

--- 00005_Nuevo año/test.py
+++ 00005_Nuevo año/test.py
@@ -1,12 +1,12 @@
 class Test(unittest.TestCase):
 
-  def test_nuevo_anio(self):
+  def test_si_invocamos_nuevo_anio_se_debe_incrementar_en_uno_anio_actual(self):
     global anio_actual
     anio_actual = 2020
     nuevo_anio()
     self.assertEqual(anio_actual, 2021)
     
-  def test_nuevo_anio_2(self):
+  def test_si_invocamos_nuevo_anio_tres_veces_se_debe_incrementar_en_tres_anio_actual(self):
     global anio_actual
     anio_actual = 2020
     nuevo_anio()
